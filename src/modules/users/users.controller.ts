10211244aeee86848db41aebb341499a8b0e3e16
Nicolas Zwierzykowski <nicolaszwier@gmail.com>
2024-05-05 20:23:31 -0400

    new: users module

---
+++ src/modules/users/users.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { ActiveUserId } from 'src/shared/decorators/ActiveUserId';
+
+@Controller('api/users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get('/my-profile')
+  me(@ActiveUserId() userId: string) {
+    return this.usersService.getUserById(userId);
+  }
+}
