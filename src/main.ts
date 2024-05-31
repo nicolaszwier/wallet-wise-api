f09fdc705dd9f4913316b55735e52c4a0a180c10
Nicolas Zwierzykowski <nicolaszwier@gmail.com>
2024-05-31 15:20:31 -0400

    deployment test

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,8 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({ origin: '*' });
 
+  console.log("deployment test")
+
   await app.listen(process.env.PORT || 3000);
 }
 
