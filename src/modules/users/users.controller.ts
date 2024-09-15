a80eaa1f0c8c7b3ebb61935b5545e7b4996830e0
Nicolas Zwierzykowski <nicolaszwier@gmail.com>
2024-09-15 14:50:06 -0400

    feat: sort categories

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, HttpStatus, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { ActiveUserId } from 'src/shared/decorators/ActiveUserId';
 import { IsPublic } from 'src/shared/decorators/IsPublic';
+import { CreateUserSupportDto } from './dto/create-support.dto';
 
 @Controller('api/users')
 export class UsersController {
@@ -10,6 +11,13 @@ export class UsersController {
   @Get('/my-profile')
   me(@ActiveUserId() userId: string) {
     return this.usersService.getUserById(userId);
+  }
+
+  @IsPublic()
+  @Post('/support')
+  support(@Body() createUserSupportDto: CreateUserSupportDto[]) {
+    throw new HttpException('Something wrong happened', HttpStatus.BAD_REQUEST);
+    // return this.transactionsService.createMany(userId, createTransactionDto);
   }
 
   @IsPublic()
