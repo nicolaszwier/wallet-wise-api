d07baef5363d306f2249deb3a28cf4365e353c30
Nicolas Zwierzykowski <nicolaszwier@gmail.com>
2026-06-09 19:21:44 -0300

    fix: fix error on crypto package

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import 'dotenv/config';
 
+import { webcrypto } from 'node:crypto';
+
+if (!globalThis.crypto) {
+  globalThis.crypto = webcrypto as Crypto;
+}
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
